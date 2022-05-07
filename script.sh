4134f40ff51d028165327fe0aa403a1a7cfc8724
Janghyun Son <jhson989@gmail.com>
2022-05-07 23:24:09 +0900

    update

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 make
-./conv_fwd.out 1 3 128 128 6 3 3 1 1 1 1
-./conv_fwd.out 4 3 128 128 6 3 3 1 1 1 1
-./conv_fwd.out 16 3 128 128 6 3 3 1 1 1 1
-./conv_fwd.out 64 3 128 128 6 3 3 1 1 1 1
-./conv_fwd.out 256 3 128 128 6 3 3 1 1 1 1
+
+# file batch IC IH IW OC FH FW PH PW SH SW
+./conv_fwd.out 1024 3 128 128 6 1 1 0 0 1 1
 ./conv_fwd.out 1024 3 128 128 6 3 3 1 1 1 1
+./conv_fwd.out 1024 3 128 128 6 5 5 2 2 1 1
+./conv_fwd.out 1024 3 128 128 6 7 7 3 3 1 1
+./conv_fwd.out 1024 3 128 128 6 9 9 4 4 1 1
